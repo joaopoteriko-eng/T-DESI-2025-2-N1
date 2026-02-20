fd776d54b08e7d7b5095ae8b59957d4aeac0f90a
joaopoteriko-eng <joao.pteriko@gmail.com>
2026-02-19 21:28:12 -0300

    quarto commit

---
+++ modelos.py
@@ -0,0 +1,44 @@
+'''Atividade de programação de aplicativo
+
+Fábrica de Chocolate
+
+Cliente
+
+
+Atributos (o que tem)                                       PORQUE?
+
+1 Atributo → nome_empresa                       Identificar o nome da empresa
+
+
+2 Atributo → cnpj                                         Garantir a identificação fiscal e legal da                         
+                                                                 empresa no sistema
+
+3 Atributo → telefone                                   Para poder se comunicar com o cliente
+
+
+4 Atributo → endereco                                Saber onde a empresa se encontra
+
+
+5 Atributo → email                                      
+                                                    Para o envio de notas fiscais, confirmação
+                                                       de pedidos e comunicação formal
+
+Comportamentos (o que faz)                             PORQUE?
+
+1 Comportamento → fazer_pedido                        Para que o cliente possa realizar              
+                                                                              seu pedido  
+
+2 Comportamento → cancelar_pedido                   Para que o cliente possa realizar o 
+                                                                              cancelamento do pedido
+
+3 Comportamento → atualizar_cadastro                Para que o cliente possa alterar
+                                                                              seu telefone, endereço e e-mail.
+
+4 Comportamento → realizar_pagamento              Para pagar o pedido que acabou 
+                                                                              de fazer
+
+5 Comportamento → consultar_historico               Para que o cliente possa consultar
+                                                  o histórico de pedidos que fez
+
+aluno:João Gabriel Poteriko
+'''
